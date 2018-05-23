54f34dc94f488486a74e3e025f7ca088dd0071c7
dddyvonne <37880913+dddyvonne@users.noreply.github.com>
2018-05-23 12:20:04 -0400

    Update seesaw_Newsfeed_20180523.R

--- seesaw_Newsfeed_20180523.R
+++ seesaw_Newsfeed_20180523.R
@@ -16,8 +16,8 @@ library(V8)
 library(readxl)
 #cat("\014")
 
-news <- read_excel("~/Desktop/daily News .xlsx")
-twitter <- read_excel("~/Desktop/dailyTwitter.xlsx")
+news <- read_excel("~/data/daily News .xlsx")
+twitter <- read_excel("~/data/dailyTwitter.xlsx")
 twitter <- data.frame(twitter)
 
 b <- gsub(".*www.", "", news$Link, perl = TRUE)
